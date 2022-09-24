2c92e627205ed5549b69bdfd3668000b11d00662
UnderWaterWelder27 <vvlad1175@gmail.com>
2022-09-24 23:25:37 +0300

    completed login form (frontend in progress)

---
+++ Login-Form/Login-Form/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Login_Form.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Login_Form.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+        public IActionResult Check(UserModel model)
+        {
+            if (model.Username == null || model.Password == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var user = GetUsers().Where(u => u.Username.Equals(model.Username))
+                                 .Where(u => u.Password.Equals(model.Password))
+                                 .FirstOrDefault();
+            if (user != null)
+            {
+                model = user;
+                model.IsLogged = true;
+            }
+            return View(model);
+        }
+        public List<UserModel> GetUsers()
+        {
+            return new List<UserModel>
+            {
+                new UserModel() { Username="Vladik", Password="007", IsAdmin=false },
+                new UserModel() { Username="Admin", Password="228", IsAdmin=true }
+            };
+        }
+    }
+}
---
+++ Login-Form/Login-Form/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Login_Form.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsLogged { get; set; }
+    }
+}
